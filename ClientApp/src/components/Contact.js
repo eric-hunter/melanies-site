59f2443c1a9485ebeab80902b6e2d1c8717fec45
eric-hunter <integratedchaos1@gmail.com>
2019-09-19 16:49:01 -0400

    Working on front-end of contact form. Close to finished.

--- ClientApp/src/components/Contact.js
+++ ClientApp/src/components/Contact.js
@@ -8,63 +8,63 @@ import './Contact.css';
 export class Contact extends Component {
   displayName = Contact.name
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            name: ""
-        };
-
-    }
-
     componentDidMount() {
         //react must remove everything when you leave a component. idk
-        window.FB.XFBML.parse();
+        //https://stackoverflow.com/questions/37795346/how-to-load-facebook-page-plugin-with-react
+        //some people's security settings affect CORS so FB nullibility needs to be checked.
+        window.FB && window.FB.XFBML.parse();
     }
 
-  render() {
-    return (
-        <div>
-            <br/>
-            <br/>
-            <Row>
-                <Col >
-                    <div className='header'>
-                        Contact Me
-                    </div>
-                </Col>
-            </Row>
-            <br/>
-            <br/>
-            <Row>
-                <Col lg='4' sm='12'>
-                    <ContactForm>
-                    </ContactForm>
-                </Col>
+    render() {
+        return (
+            <div>
                 <br/>
-                <Col lg='4' md='6'>
+                <br/>
+                <Row>
+                    <Col >
+                        <div className='header'>
+                            Contact Me
+                        </div>
+                    </Col>
+                </Row>
+                <br/>
+                <br/>
+                <Row>
+                    <Col lg='4' sm='12'>
+                        <ContactForm>
+                        </ContactForm>
+                    </Col>
+                    <br/>
+                    <Col lg='4' md='6'>
 
-                    <div 
-                        class="fb-page" 
-                        data-href="https://www.facebook.com/ashapridealliance/" 
-                        data-tabs="timeline" 
-                        data-width="" 
-                        data-height="" 
-                        data-small-header="false" 
-                        data-adapt-container-width="true" 
-                        data-hide-cover="false" 
-                        data-show-facepile="true"><blockquote cite="https://www.facebook.com/ashapridealliance/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/ashapridealliance/">Asha Pride Alliance</a></blockquote></div>
-                </Col>
-                <Col lg='4' md='6'>
-                    <div class='side-panel'>
-                        <br/><br/>
-                        <Glyphicon glyph='glyphicon glyphicon-phone'></Glyphicon> <span>  (678)  632 - 3837</span>
-                        <br/><br/>
-                        <Glyphicon glyph='glyphicon glyphicon-envelope'></Glyphicon><span>  ashapridealliance@gmail.com</span>
-                    </div>
-                </Col>
-            </Row>
-        </div>
-    );
-  }
+                        <div 
+                            class="fb-page" 
+                            data-href="https://www.facebook.com/ashapridealliance/" 
+                            data-tabs="timeline" 
+                            data-width="" 
+                            data-height="" 
+                            data-small-header="false" 
+                            data-adapt-container-width="true" 
+                            data-hide-cover="false" 
+                            data-show-facepile="true"><blockquote cite="https://www.facebook.com/ashapridealliance/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/ashapridealliance/">Asha Pride Alliance</a></blockquote></div>
+                    </Col>
+                    <Col lg='4' md='6'>
+                        <div class='side-panel'>
+                            <br/><br/>
+                            <Glyphicon glyph='glyphicon glyphicon-phone'></Glyphicon> 
+                            <span>  
+                                <a href='tel:(678) 632-3837'>(678) 632-3837</a>
+                            </span>
+                            <br/><br/>
+                            {/*Add clipboard thingy*/}
+                            <Glyphicon glyph='glyphicon glyphicon-envelope'></Glyphicon>
+                            <span>  
+                                <a href='mailto:ashapridealliance@gmail.com'>ashapridealliance@gmail.com</a>
+                            </span>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
 }
--- ClientApp/src/components/ContactForm.js
+++ ClientApp/src/components/ContactForm.js
@@ -15,17 +15,38 @@ export class ContactForm extends Component {
 
         this.state = {
             name: "",
-            reason: "",
+            interest: "",
             email: "",
-            phone: ""
+            phone: "",
+            preference: ""
         };
 
         this.changeName = this.changeName.bind(this);
+        this.changeInterest = this.changeInterest.bind(this);
+        this.changeEmail = this.changeEmail.bind(this);
+        this.changePhone = this.changePhone.bind(this);
+        this.changePreference = this.changePreference.bind(this);
     }
 
     //TODO: make dynamic instead of function for ever form field.
     changeName(e) {
         this.setState({ name: e.target.value });
+    }
+
+    changeInterest(e) {
+        this.setState({ interest: e.target.value });
+    }
+
+    changeEmail(e) {
+        this.setState({ email: e.target.value });
+    }
+
+    changePhone(e) {
+        this.setState({ phone: e.target.value });
+    }
+
+    changePreference(e) {
+        this.setState({ preference: e.target.value });
     }
 
     render() {
@@ -35,39 +56,76 @@ export class ContactForm extends Component {
                     <Col lg='1'>
                     </Col>
                     <Col lg='11'>
-                        <FormGroup>
-                            <ControlLabel>Name</ControlLabel>
-                            <FormControl
-                                type='text'
-                                value={this.state.name}
-                                onChange={this.changeName} />
-
-                        </FormGroup>
-                    </Col>
-                    
-                </Row>
-                <Row>
-                    <Col lg='1'/>
-                    <Col lg='11'>
-                        <FormGroup>
-                            <ControlLabel>Reason For Contacting</ControlLabel>
-                            <FormControl
-                                type=''
-                                value={this.state.name}
-                                onChange={this.changeName} />
-                        </FormGroup>
+                        <div class='form-group'>
+                            <label for='name'>Name</label>
+                            <br/>
+                            <input id='name' type='text' value={this.state.name} onChange={this.changeName}/>
+                        </div>
                     </Col>
                 </Row>
                 <Row>
                     <Col lg='1'/>
                     <Col lg='11'>
-                        <FormGroup>
-                            <ControlLabel>Email</ControlLabel>
-                            <FormControl
-                                type='text'
-                                value={this.state.name}
-                                onChange={this.changeName} />
-                        </FormGroup>
+                        <label for='interest'>Reason</label>
+                        <select id='interest' class='form-control' value={this.state.interest} onChange={this.changeInterest}>
+                            <option value='None'>-Select-</option>
+                            <option value='Book Consultation'>Book Consultation</option>
+                            <option value='Appointment'>Appointment</option>
+                            <option value='Presentation'>Presentation (ex. workplace/school)</option>
+                            <option value='Group and Individual'>Group or Individual???</option>
+                        </select>
+                        <br/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg='1'/>
+                    <Col lg='11'>
+                        <div class='form-group'>
+                            <label for='email'>Email</label>
+                            <br/>
+                            <input id='email' type='text' value={this.state.email} onChange={this.changeEmail}/>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg='1'/>
+                    <Col lg='11'>
+                        <div class='form-group'>
+                            <label for='phone'>Phone</label>
+                            <br/>
+                            <input id='phone' type='text' value={this.state.phone} onChange={this.changePhone}/>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg='1'/>
+                    <Col lg='11'>
+                        <div class='form-group'>
+                            <label for='preference'>Contact Preference</label>
+                            <br/>
+                            <select id='preference' class='form-control' value={this.state.preference} onChange={this.changePreference}>
+                                <option>-Select-</option>
+                                <option>Phone</option>
+                                <option>Email</option>
+                            </select>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                <Col lg='1'/>
+                    <Col lg='11'>
+                        <div class='form-group'>
+                            <label for='message'>Message</label>
+                            <br/>
+                            <textarea 
+                                id='message' 
+                                class='form-control' 
+                                value={this.state.message} 
+                                onChange={this.changeMessage}
+                                rows={4}
+                                cols={100}>
+                            </textarea>
+                        </div>
                     </Col>
                 </Row>
                 <Row>
