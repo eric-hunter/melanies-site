b81b3453bc77cda9bad99d036cdf03b6ab8a3fcd
eric-hunter <integratedchaos1@gmail.com>
2019-10-25 18:41:57 -0400

    Testing azure remote repo.

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,5 @@
-using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
